948184f6e452db9ebf960f384690494132c9d232
Mati <matiboux@gmail.com>
2023-12-24 06:51:30 +0100

    Add description (#8)
    Closes #4

--- app/app/src/i18n/locales/en.ts
+++ app/app/src/i18n/locales/en.ts
@@ -3,6 +3,8 @@ const defaultLocale =
 	'What is {0}?',
 	'Visit our website',
 	'Join our Discord',
+	'You like games? This platform will be a way to make accessible to all our members a number of games in a few clicks without having to download anything!',
+	'A platform of games between friends allowing you to create your lobby and to share a moment together on a list of games and tools that we created and/or curated',
 ] as const
 
 type Keys = typeof defaultLocale[number]
